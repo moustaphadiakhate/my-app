35609b4e04902b49159dcbe6e69de35ce5dfca35
Moustapha <moustaphadiakhate1212@gmail.com>
2024-04-25 19:36:59 +0000

    [00] itialize chp13 NIIT Dakar

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+app.use((req, res, next) => {
+    const logData = {
+        method: req.method,
+        url: req.url,
+        timestamp: new Date().toISOString()
+    };
+    console.log(JSON.stringify(logData));
+    next();
+});
+
 app.use(express.static(__dirname + '/dist/my-app'));
 
 app.get('/*', function(req,res) {
+
     res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
 
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: 'https://my-staging-url',
+  deployed_env: 'dev'
+};
